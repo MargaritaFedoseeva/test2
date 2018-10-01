5be1a9b68337a7946b15482bc5f855007cac0511
ruafem4 <ZXcvbn5890>
2018-10-01 12:53:20 +0300

    Обработка исключений

--- src/com/company/Present.java
+++ src/com/company/Present.java
@@ -1,28 +1,33 @@
 package com.company;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Present {
     public static void main(String[] args) {
         System.out.println("Введите вес конфет в граммах.");
         Scanner scanner = new Scanner(System.in);
+        boolean bError = true;
         Sweet sweet = new Sweet();
         int weight = 0;
+
         Sweet[] present = {new Chocolates("Шоколадные", 250, weight), new Jellybean("Жевательные", 360, weight), new Lollipop("Леденцы", 300, weight)};
         for (int i = 0; i < present.length; i++) {
             System.out.print(present[i].getTitle() + ":");
-            try {
-                weight = scanner.nextInt();
-            } catch (Exception e) {
-                System.out.print("Возникло иключение" + e);
-            }
-
+            do {
+                try {
+                    weight = scanner.nextInt();
+                    bError = true;
+                } catch (Exception e) {
+                    System.out.println("Вы ввели некорректное значение, ошибка: " + e);
+                    System.out.print("Введите значение заново: ");
+                    scanner.next();
+                    bError = false;
+                }
+            } while (!bError);
             present[i].setWeight(weight);
             present[i].priceGram();
-
         }
-
-
         sweet.totalWeightAndCost();
         System.out.println("Информация по позициям");
         sweet.dataOutput(present);
@@ -44,7 +49,7 @@ public class Present {
 //
 //        }
         scanner.close();
-}
+    }
 
 
 }
