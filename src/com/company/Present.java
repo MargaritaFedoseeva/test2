2cbd4d1ea3ca0078c3743f95713edb18c8ef1179
ruafem4 <ZXcvbn5890>
2018-10-12 15:50:09 +0300

    Добавила в подарки вывод и сортировку конфет с помощью stream.

--- src/com/company/Present.java
+++ src/com/company/Present.java
@@ -3,6 +3,8 @@ package com.company;
 import java.io.*;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Present {
     public static void main(String[] args) {
@@ -28,7 +30,7 @@ public class Present {
             BufferedReader reader = new BufferedReader(new FileReader("C:\\project-testing\\Sweets.txt"));
             while ((line = reader.readLine()) != null) {
                 String[] strM = line.split(",");
-               String name=strM[0].replaceAll(String.valueOf('\uFEFF'), "");
+                String name = strM[0].replaceAll(String.valueOf('\uFEFF'), "");
                 switch (name) {
                     case "Шоколад":
                         present.add(new Chocolates(name, Double.valueOf(strM[1]), Double.valueOf(strM[2])));
@@ -52,10 +54,11 @@ public class Present {
         System.out.println("Информация по позициям");
         present.stream()
 //              .filter(sweet -> sweet.getTitle().contains("Шоколад"))
-                .forEach(sweet ->{
-                    totalWeight.set(sweet.getWeight()+totalWeight.get());
-                    totalCost.set(sweet.priceGram()+totalCost.get());
-                        System.out.printf("%s: %.2f руб. - стоимость за %.2f г, %.2f руб. цена за кг \n", sweet.getTitle(), sweet.getPriseGram(), sweet.getWeight(), sweet.getPrice());});
+                .forEach(sweet -> {
+                    totalWeight.set(sweet.getWeight() + totalWeight.get());
+                    totalCost.set(sweet.priceGram() + totalCost.get());
+                    System.out.printf("%s: %.2f руб. - стоимость за %.2f г, %.2f руб. цена за кг \n", sweet.getTitle(), sweet.getPriseGram(), sweet.getWeight(), sweet.getPrice());
+                });
 
         System.out.println("Общий вес:" + totalWeight);
         System.out.println("Общая цена:" + totalCost);
@@ -77,6 +80,5 @@ public class Present {
         } while (!bError);
         return weight;
     }
-
 
 }
